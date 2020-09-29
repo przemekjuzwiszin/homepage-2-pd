350a14dad8765ff57a41dba33181d10e29b0fd11
Przemek Juzwiszin <przemekjuzwiszin@gmail.com>
2020-09-29 13:27:26 +0200

    use document.querySelector to show element h1

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ console.log(age);
 
 console.log(`Nazywam się ${name} i mam ${age} lata`);
 console.log('Miło Cię widzieć, skoro tu zaglądasz, nie jest to przypadek. Idziesz w dobrym kierunku. Coś już umiesz bravo')
+
+const header = document.querySelector('.main__heading');
+console.log(header);
