9875d6841742b653e058ebe6295426e2991805fd
Przemek Juzwiszin <przemekjuzwiszin@gmail.com>
2020-12-31 17:31:03 +0100

    change button to x when menu is visible

--- main.js
+++ main.js
@@ -29,4 +29,9 @@ const navigationSwitcher = document.querySelector(".navigation__switcher--js");
 navigationSwitcher.addEventListener("click", (e) => {
   const navigationList = document.querySelector(".navigation__list--js");
   navigationList.classList.toggle("navigation__list--visible");
+  if (navigationList.classList.contains('navigation__list--visible')) {
+    navigationSwitcher.innerHTML = '⨯';
+  } else {
+    navigationSwitcher.innerHTML = '≡';
+  }
 });
