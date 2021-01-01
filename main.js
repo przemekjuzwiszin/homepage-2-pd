f1ae696281e5684afb934cebc3fe90057d283ffd
Przemek Juzwiszin <przemekjuzwiszin@gmail.com>
2021-01-01 16:42:32 +0100

    Correct the typo bravo to brawo

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ console.log(age);
 
 console.log(`Nazywam się ${name} i mam ${age} lata`);
 console.log(
-  "Miło Cię widzieć, skoro tu zaglądasz, nie jest to przypadek. Idziesz w dobrym kierunku. Coś już umiesz bravo"
+  "Miło Cię widzieć, skoro tu zaglądasz, nie jest to przypadek. Idziesz w dobrym kierunku. Coś już umiesz brawo."
 );
 
 const header = document.querySelector(".main__heading-js");
